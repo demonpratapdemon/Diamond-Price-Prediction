818933be410259dea5f65ec1bc0860f93bd8dcf1
demonpratapdemon <pratapkumarchandra.96@gmail.com>
2024-08-10 15:56:41 +0530

    Adding the pickle file

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ from src.logger import logging
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
+        print(file_path)
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
