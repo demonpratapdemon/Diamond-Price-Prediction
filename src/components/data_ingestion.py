35c4247c540cc8f63bf0c1e1202f8c903b582841
demonpratapdemon <pratapkumarchandra.96@gmail.com>
2024-08-10 13:13:09 +0530

    Adding data ingestion and data transformation and tirggering from pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,35 +9,42 @@ from dataclasses import dataclass
 
 # Initialize the Data Ingestion Configuration
 
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str = os.path.join('artifacts', 'train.csv')
-    test_data_path:str = os.path.join('artifacts', 'test.csv')
-    raw_data_path:str = os.path.join('artifacts', 'raw.csv')
-    
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "raw.csv")
+
 
 # Create the Data Intgestion class
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
-        
+
     def initiate_data_ingestion(self):
         logging.info("Data Ingestion method starts")
         try:
-            df = pd.read_csv(filepath_or_buffer="./../../notebooks/data/gemstone.csv")
+            df = pd.read_csv(filepath_or_buffer="notebooks/data/gemstone.csv")
             logging.info("Dataset read as Pandas dataframe")
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True
+            )
             df.to_csv(self.ingestion_config.raw_data_path, index=False)
             logging.info("Raw data is created")
             train_set, test_set = train_test_split(df, test_size=0.3, random_state=42)
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            train_set.to_csv(
+                self.ingestion_config.train_data_path, index=False, header=True
+            )
+            test_set.to_csv(
+                self.ingestion_config.test_data_path, index=False, header=True
+            )
             logging.info("Ingestion of data is completed!")
             return (
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+                self.ingestion_config.test_data_path,
             )
-            
+
         except Exception as e:
             logging.info("Exception occured at Data Ingestion Stage")
             raise CustomException(e, sys)
---
+++ src/components/data_transformation.py
@@ -0,0 +1,140 @@
+from sklearn.impute import SimpleImputer  ## HAndling Missing Values
+from sklearn.preprocessing import StandardScaler  # HAndling Feature Scaling
+from sklearn.preprocessing import OrdinalEncoder  # Ordinal Encoding
+
+## pipelines
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+import numpy as np
+import pandas as pd
+import sys, os
+from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join("./../../artifacts", "preprocessor.pkl")
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformation_object(self):
+        try:
+            logging.info("Data transformation initiated")
+
+            # Categorical and numerical columns
+            categorical_cols = ["cut", "color", "clarity"]
+            numerical_cols = ["carat", "depth", "table", "x", "y", "z"]
+
+            # Define the custom ranking for each ordinal variable
+            cut_categories = ["Fair", "Good", "Very Good", "Premium", "Ideal"]
+            color_categories = ["D", "E", "F", "G", "H", "I", "J"]
+            clarity_categories = [
+                "I1",
+                "SI2",
+                "SI1",
+                "VS2",
+                "VS1",
+                "VVS2",
+                "VVS1",
+                "IF",
+            ]
+
+            logging.info("Data Transformation Pipeline initiated.")
+
+            ## Numerical Pipeline
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler()),
+                ]
+            )
+
+            # Categorigal Pipeline
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    (
+                        "ordinalencoder",
+                        OrdinalEncoder(
+                            categories=[
+                                cut_categories,
+                                color_categories,
+                                clarity_categories,
+                            ]
+                        ),
+                    ),
+                    ("scaler", StandardScaler()),
+                ]
+            )
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_cols),
+                    ("cat_pipeline", cat_pipeline, categorical_cols),
+                ]
+            )
+
+            logging.info("Data Transformation completed!")
+
+            return preprocessor
+
+        except Exception as e:
+            logging.info("Exception occured in data transformation")
+            raise CustomException(e, sys)
+
+    def initiate_data_transformation(self, train_data_path, test_data_path):
+        try:
+            train_df = pd.read_csv(train_data_path)
+            test_df = pd.read_csv(test_data_path)
+
+            logging.info("Reading of train and test data completed")
+            logging.info(f"Train DataFrame Head : \n{train_df.head().to_string()}")
+            logging.info(f"Test DataFrame Head : \n{test_df.head().to_string()}")
+
+            logging.info("Fetching Data Transformation object")
+            preprocessor_obj = self.get_data_transformation_object()
+
+            target_column = "price"
+            drop_column = ["id", target_column]
+
+            # Dividing the dataset into independent and dependent features for train data
+            input_feature_train_df = train_df.drop(columns=drop_column, axis=1)
+            target_feature_train_df = train_df[target_column]
+
+            # Dividing the dataset into independent and dependent features for test data
+            input_feature_test_df = test_df.drop(columns=drop_column, axis=1)
+            target_feature_test_df = test_df[target_column]
+
+            input_feature_train_arr = preprocessor_obj.fit_transform(
+                input_feature_train_df
+            )
+            input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
+
+            logging.info("Applying preprocessing on training and test data completed!")
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            # save the arrays
+            save_object(
+                self.data_transformation_config.preprocessor_obj_file_path,
+                preprocessor_obj,
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        except Exception as e:
+            logging.info("Error occured while performing data transformation")
+            raise CustomException(e, sys)
